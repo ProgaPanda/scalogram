65984daecfe33f7de24c8baa83e892cfa8a12be1
Khaled Alba <looda97@gmail.com>
2019-06-19 15:20:48 +0300

    New-weight-btn init

---
+++ src/components/new-weight-btn.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <v-btn class="new-weight-btn" color="#19ffd6" large round fixed bottom>new weight</v-btn>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.new-weight-btn {
+  z-index: 1000;
+  left: 50%;
+  transform: translateX(-50%);
+}
+</style>
